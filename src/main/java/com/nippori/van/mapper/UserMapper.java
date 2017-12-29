92b42bf804adf3d961f105c149db502113d6ace8
yeahCaesar <125360167@qq.com>
2017-12-30 01:30:37 +0800

    大规模更新

--- src/main/java/com/nippori/van/mapper/UserMapper.java
+++ src/main/java/com/nippori/van/mapper/UserMapper.java
@@ -4,11 +4,13 @@ import com.nippori.van.bean.User;
 import org.apache.ibatis.annotations.Result;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by Caesar
  * on 2017-11-16.
  */
+@Repository
 public interface UserMapper {
     @Select("SELECT * FROM EAUSER WHERE userid = #{id}")
     @Results({
