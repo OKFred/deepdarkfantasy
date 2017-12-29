d139da3804d68a6916b95f18230e71d437daa608
yeahCaesar <125360167@qq.com>
2017-12-30 01:14:14 +0800

    大规模更新

--- src/main/java/com/nippori/van/controller/VisitPageContriller.java
+++ src/main/java/com/nippori/van/controller/VisitPageContriller.java
@@ -3,6 +3,8 @@ package com.nippori.van.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Map;
+
 /**
  * Created by Caesar
  * on 2017-11-16.
@@ -12,7 +14,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class VisitPageContriller {
 
     @RequestMapping("")
-    public String welcomePage(){
+    public String welcomePage(Map<String,Object> map){
+        map.put("hello","hello world");
         System.out.println("访问经过了分发...");
         return "index";
     }
